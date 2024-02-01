fc414ce08c60da6af251d9ddeb815823853d3d45
SSQ <110625524+ssq0-0@users.noreply.github.com>
2024-02-01 22:22:58 +0300

    Register new models in admin.py

--- siteUcheBudka/HW/admin.py
+++ siteUcheBudka/HW/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import HW
+from .models import HW, Teacher, Student, StudentHW
 
 
 admin.site.register(HW)
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(StudentHW)
