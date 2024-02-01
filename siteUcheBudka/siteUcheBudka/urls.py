c85dcfc7a2a8ca6a9e3c6e0d20a37387c6a98fdd
SSQ <110625524+ssq0-0@users.noreply.github.com>
2024-02-01 22:23:47 +0300

    Added new urls in  urls.py

--- siteUcheBudka/siteUcheBudka/urls.py
+++ siteUcheBudka/siteUcheBudka/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 from HW.views import HWAPIView
 
 
@@ -7,4 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/HW/', HWAPIView.as_view()),
     path('api/v1/HW/<int:pk>/', HWAPIView.as_view()),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
