c1e9bec58f090d80b18d257c0aa98652f1b9f72c
SSQ <110625524+ssq0-0@users.noreply.github.com>
2024-02-01 22:25:08 +0300

    Update serializers.py

--- siteUcheBudka/HW/serializers.py
+++ siteUcheBudka/HW/serializers.py
@@ -1,19 +1,18 @@
 from rest_framework import serializers
-from .models import HW
+from .models import HW, Teacher
 from django.utils import timezone
 
 
 class HWSerializers(serializers.Serializer):
-    # class Meta:
-    #     model = HW
-    #     fields = '__all__'
+    """Класс сериализатор для преобразования данных в JSON и обратно"""
     school_subject = serializers.CharField(max_length=50)
     task_text = serializers.CharField()
     fact_answer = serializers.CharField()
     # publish_time = serializers.DateTimeField()
 
     def create(self, validated_data):
-        return HW.objects.create(**validated_data)
+        teacher = self.context['request'].user.teacher
+        return HW.objects.create(teacher=teacher, **validated_data)
 
     def update(self, instance, validated_data):
         instance.school_subject = validated_data.get('school_subject')
