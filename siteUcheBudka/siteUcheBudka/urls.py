c5f26a156cb10edbf725f6385b7a997ee48b1ddd
SSQ <110625524+ssq0-0@users.noreply.github.com>
2024-02-16 14:46:05 +0300

    Update urls.py
    New urls

--- siteUcheBudka/siteUcheBudka/urls.py
+++ siteUcheBudka/siteUcheBudka/urls.py
@@ -1,22 +1,22 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from HW.views import login_, StudentAPIView, HWAPIModelViewSet, HWParamsAPIView, main_
 
-from HW.views import HWAPIView, login_, test_, HWAPIUpdateView, HWAPIDestroyView, StudentAPIView, HWAPIPostView
 
-
+router = routers.SimpleRouter()
+router.register(r'HW', HWAPIModelViewSet)
+print(router.urls)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/HW/', HWAPIGetView.as_view()),
-    path('api/v1/HW/<int:pk>/', HWAPIGetView.as_view()),
-    path('api/v1/HW/edit/<int:pk>/', HWAPIUpdateView.as_view()),
-    path('api/v1/HW/create/', HWAPIPostView.as_view()),
-    path('api/v1/HW/delete/<int:pk>/', HWAPIDestroyView.as_view()),
-    path('api/v1/HW/answer/<int:pk>/', StudentAPIView.as_view()),
+    path('api/v1/', include(router.urls)),
+    path('api/v1/mainHW/', HWParamsAPIView.as_view()),
+    path('api/v1/HW/answer/<int:pk>', StudentAPIView.as_view()),
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/v1/login-page/', login_, name='hw'),
-    path('api/v1/test/', test_, name='test')
+    path('api/v1/main/', main_, name='main')
 ]
