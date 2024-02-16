cb57acab49860599d57370bcb6801549b8dae749
SSQ <110625524+ssq0-0@users.noreply.github.com>
2024-02-16 14:33:13 +0300

    Update admin.py
    New models & realisation

--- siteUcheBudka/HW/admin.py
+++ siteUcheBudka/HW/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import HW, Teacher, Student, StudentHW
+from .models import HW, Teacher, Student, StudentHW, StudentClass
+
+
+class StudentClassAdmin(admin.ModelAdmin):
+    """Класс для более удобного администрирования модели"""
+    list_display = ['class_number']
+    filter_horizontal = ['students']
 
 
 admin.site.register(HW)
 admin.site.register(Teacher)
 admin.site.register(Student)
 admin.site.register(StudentHW)
+admin.site.register(StudentClass, StudentClassAdmin)
