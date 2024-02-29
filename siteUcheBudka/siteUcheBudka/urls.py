0c2b5d903832f8bbe4a40c7610040e6c0536c2f0
SSQ <110625524+ssq0-0@users.noreply.github.com>
2024-02-29 22:34:25 +0300

    Update urls.py

--- siteUcheBudka/siteUcheBudka/urls.py
+++ siteUcheBudka/siteUcheBudka/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
-from HW.views import login_, StudentAPIView, HWAPIModelViewSet, HWParamsFroStudentAPIView, TeacherMarkAPIViewSet, UserRegistrationAPIView, DiaryAPIView, main_, HWPage_, Diary_
+from HW.views import login_, StudentAPIView, HWAPIModelViewSet, HWParamsFroStudentAPIView, ProfileAPIView, TeacherMarkAPIViewSet, UserRegistrationAPIView, DiaryAPIView, main_, HWPage_, Diary_, lk_
 
 
 router = routers.SimpleRouter()
-router.register(r'HW', HWAPIModelViewSet)
+router.register(r'HW', HWAPIModelViewSet, basename='HWAPIModelViewSet')
 print(router.urls)
 
 urlpatterns = [
@@ -14,6 +14,7 @@ urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('api/v1/registrations/', UserRegistrationAPIView.as_view()),
     path('api/v1/mainHW/', HWParamsFroStudentAPIView.as_view()),
+    path('api/v1/profile/', ProfileAPIView.as_view()),
     path('api/v1/SetMarkHW/', TeacherMarkAPIViewSet.as_view()),
     path('api/v1/HW/answer/<int:pk>/', StudentAPIView.as_view()),
     path('api/v1/my_hw/', DiaryAPIView.as_view()),
@@ -23,5 +24,6 @@ urlpatterns = [
     path('api/v1/login-page/', login_, name='hw'),
     path('api/v1/main/', main_, name='main'),
     path('api/v1/HWPage/', HWPage_, name='HWPage'),
-    path('api/v1/Diary/', Diary_, name='diary')
+    path('api/v1/Diary/', Diary_, name='diary'),
+    path("api/v1/lk/", lk_, name='lk'),
 ]
