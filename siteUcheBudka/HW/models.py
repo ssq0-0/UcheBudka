52da9d8aa2b9687559607b0afd25546d1dc0e4f3
SSQ <110625524+ssq0-0@users.noreply.github.com>
2024-02-29 22:33:12 +0300

    Update models.py

--- siteUcheBudka/HW/models.py
+++ siteUcheBudka/HW/models.py
@@ -1,80 +1,69 @@
-from rest_framework import serializers
-from .models import HW, Teacher, StudentHW, StudentClass, Profile, Student
-from django.utils import timezone
+"""Модели студента, препода, домашней работы. Класс StudentHW хранит в себе домашнюю работу студента для
+дальнейшей проверки с ответом преподавателя.
+
+Класс HW - общий класс для хранения домашней работы, которую публикует преподаватель, ее может видеть любой,
+в том числе и не авторизованный пользователь"""
+from django.db import models
 from django.contrib.auth.models import User
 
 
-class HWSerializers(serializers.Serializer):
-    """Класс сериализатор для преобразования данных в JSON и обратно"""
-    school_subject = serializers.CharField(max_length=50)
-    task_text = serializers.CharField()
-    fact_answer = serializers.CharField()
-    student_class = serializers.PrimaryKeyRelatedField(queryset=StudentClass.objects.all())
-    id = serializers.CharField(max_length=30)
-    # publish_time = serializers.DateTimeField()
-
-    def create(self, validated_data):
-        teacher = self.context['request'].user.teacher
-        return HW.objects.create(teacher=teacher, **validated_data)
-
-    def update(self, instance, validated_data):
-        instance.school_subject = validated_data.get('school_subject')
-        instance.task_text = validated_data.get('task_text')
-        instance.fact_answer = validated_data.get('fact_answer')
-        instance.publish_time = timezone.now()
-        instance.save()
-        return instance
+class StudentClass(models.Model):
+    class_number = models.CharField(max_length=10, unique=True)
+    students = models.ManyToManyField('Student', related_name='student_classes')
 
 
-class StudentHWSerializers(serializers.Serializer):
-    """Сериализатор для публикации домашки студентом. Можно дополнить функцией апдейт, что бы студент мог
-    отредачить домашку"""
-    school_subject = serializers.CharField(max_length=50, read_only=True)
-    student_answer = serializers.CharField()
-    hw = serializers.PrimaryKeyRelatedField(queryset=HW.objects.all())
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=50)
+    class_number = models.CharField(max_length=5)
+    email = models.TextField(null=True)
 
-    def create(self, validated_data):
-        student = self.context['request'].user.student
-        print("Текущий пользователь:", student)
-        # Теперь создаем экземпляр StudentHW только с ожидаемыми полями
-        return StudentHW.objects.create(student=student, complete=True, **validated_data)
+    def __str__(self):
+        return self.fullname
 
 
-class UserRegistrationSerializer(serializers.ModelSerializer):
-    """Сериализатор для реги юзеров..."""
-    password = serializers.CharField(write_only=True)
-    fullname = serializers.CharField(max_length=100, required=False)
-    class_number = serializers.CharField(max_length=10, required=False, allow_blank=True, allow_null=True)
-    subject_handling = serializers.CharField(max_length=100, required=False, allow_blank=True, allow_null=True)
-    is_teacher = serializers.BooleanField(required=False, default=False)
-    is_student = serializers.BooleanField(required=False, default=False)
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=50)
+    subject_handling = models.CharField(max_length=60)
+    email = models.TextField(null=True)
 
-    class Meta:
-        model = User
-        fields = ('username', 'password', 'fullname', 'class_number', 'subject_handling', 'is_teacher', 'is_student')
-
-    def create(self, validated_data):
-        is_teacher = validated_data.pop('is_teacher', False)
-        is_student = validated_data.pop('is_student', False)
-        fullname = validated_data.pop('fullname', '')
-        class_number = validated_data.pop('class_number', None)
-        subject_handling = validated_data.pop('subject_handling', None)
-
-        user = User.objects.create_user(**validated_data)
-
-        if is_student:
-            Student.objects.create(user=user, fullname=fullname, class_number=class_number)
-        else:
-            Teacher.objects.create(user=user, fullname=fullname, subject_handling=subject_handling)
-        Profile.objects.create(user=user, fullname=fullname, is_teacher=is_teacher, is_student=is_student)
-
-        return User
+    def __str__(self):
+        return self.fullname
 
 
-class DiarySerializer(serializers.ModelSerializer):
-    """Сериализатор для оценок"""
-    school_subject = serializers.CharField(source='hw.school_subject')
+class HW(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    student_class = models.ForeignKey(StudentClass, on_delete=models.CASCADE, null=True)
+    school_subject = models.CharField(max_length=50)
+    task_text = models.TextField()
+    fact_answer = models.TextField()
+    publish_time = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        model = StudentHW
-        fields = ('school_subject', 'mark')
+    def __str__(self):
+        return f"Предмет: {self.school_subject}, Учитель: {self.teacher}"
+
+    def is_completed(self, student):
+        return bool(StudentHW.objects.filter(hw=self, student=student, complete=True).exists())
+
+
+class StudentHW(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    hw = models.ForeignKey(HW, on_delete=models.CASCADE)
+    student_answer = models.TextField()
+    complete = models.BooleanField(default=False)
+    mark = models.IntegerField(null=True, blank=True)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_teacher = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
+    fullname = models.CharField(max_length=100)
+    class_number = models.CharField(max_length=10, blank=True, null=True)
+    subject_handling = models.CharField(max_length=100, blank=True, null=True)
+    email = models.TextField(null=True)
+
+    def __str__(self):
+        role = 'Teacher' if self.is_teacher else 'Student' if self.is_student else 'User'
+        return f"{self.user.username} ({role})"
